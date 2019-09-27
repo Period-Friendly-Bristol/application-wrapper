42eb96347fb61ae40731a0f3ac04840efb340001
herecomesjaycee <jaycee.cheong04@gmail.com>
2019-09-27 09:49:03 +0100

    Create setup file

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:PeriodFriendlyBristol/period-dignity-app-server.git server
+git clone git@github.com:PeriodFriendlyBristol/period-dignity-app-client.git client
