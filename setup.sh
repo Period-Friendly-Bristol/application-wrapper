02d37a265e85d2eb17dec318de7713460ceea3bf
Charlie <charlie.gillions@bristolisopen.com>
2020-10-20 14:43:33 +0100

    Replaced setup.sh with submodules

--- setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-git clone git@github.com:PeriodFriendlyBristol/period-dignity-app-server.git server
-git clone git@github.com:PeriodFriendlyBristol/period-dignity-app-client.git client
